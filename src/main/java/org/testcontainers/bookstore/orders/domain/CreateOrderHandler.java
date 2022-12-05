19b35cb56219df54f4d7cde1b1dbf44fc738922c
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-12-05 14:10:54 +0530

    Adding CreateOrderHandler to separate transactional scope

---
+++ src/main/java/org/testcontainers/bookstore/orders/domain/CreateOrderHandler.java
@@ -0,0 +1,35 @@
+package org.testcontainers.bookstore.orders.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.testcontainers.bookstore.ApplicationProperties;
+import org.testcontainers.bookstore.events.OrderCreatedEvent;
+import org.testcontainers.bookstore.orders.api.CreateOrderRequest;
+import org.testcontainers.bookstore.orders.domain.entity.OrderStatus;
+import org.testcontainers.bookstore.orders.domain.model.OrderConfirmationDTO;
+
+@Component
+public class CreateOrderHandler {
+    private static final Logger log = LoggerFactory.getLogger(CreateOrderHandler.class);
+
+    private final OrderService orderService;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final ApplicationProperties properties;
+
+    public CreateOrderHandler(OrderService orderService, KafkaTemplate<String, Object> kafkaTemplate, ApplicationProperties properties) {
+        this.orderService = orderService;
+        this.kafkaTemplate = kafkaTemplate;
+        this.properties = properties;
+    }
+
+    public OrderConfirmationDTO createOrder(CreateOrderRequest orderRequest) {
+        OrderConfirmationDTO orderConfirmationDTO = orderService.createOrder(orderRequest);
+        if(orderConfirmationDTO.getOrderStatus() == OrderStatus.NEW) {
+            kafkaTemplate.send(properties.newOrdersTopic(), new OrderCreatedEvent(orderConfirmationDTO.getOrderId()));
+            log.info("Published OrderCreatedEvent for orderId:{}", orderConfirmationDTO.getOrderId());
+        }
+        return orderConfirmationDTO;
+    }
+}
--- src/main/java/org/testcontainers/bookstore/orders/domain/OrderService.java
+++ src/main/java/org/testcontainers/bookstore/orders/domain/OrderService.java
@@ -2,11 +2,8 @@ package org.testcontainers.bookstore.orders.domain;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.bookstore.ApplicationProperties;
-import org.testcontainers.bookstore.events.OrderCreatedEvent;
 import org.testcontainers.bookstore.orders.api.CreateOrderRequest;
 import org.testcontainers.bookstore.orders.domain.entity.Order;
 import org.testcontainers.bookstore.orders.domain.entity.OrderStatus;
@@ -15,7 +12,6 @@ import org.testcontainers.bookstore.payment.domain.PaymentRequest;
 import org.testcontainers.bookstore.payment.domain.PaymentResponse;
 import org.testcontainers.bookstore.payment.domain.PaymentService;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -26,16 +22,11 @@ public class OrderService {
     private final PaymentService paymentService;
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
-    private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final ApplicationProperties properties;
 
-    public OrderService(PaymentService paymentService, OrderRepository orderRepository, OrderMapper orderMapper,
-                        KafkaTemplate<String, Object> kafkaTemplate, ApplicationProperties properties) {
+    public OrderService(PaymentService paymentService, OrderRepository orderRepository, OrderMapper orderMapper) {
         this.paymentService = paymentService;
         this.orderRepository = orderRepository;
         this.orderMapper = orderMapper;
-        this.kafkaTemplate = kafkaTemplate;
-        this.properties = properties;
     }
 
     public OrderConfirmationDTO createOrder(CreateOrderRequest orderRequest) {
@@ -50,33 +41,12 @@ public class OrderService {
         if(paymentResponse.getStatus() != PaymentResponse.PaymentStatus.ACCEPTED) {
             savedOrder.setStatus(OrderStatus.ERROR);
             this.updateOrderStatus(savedOrder.getOrderId(), savedOrder.getStatus(), "Payment rejected");
-        } else {
-            kafkaTemplate.send(properties.newOrdersTopic(), new OrderCreatedEvent(savedOrder.getOrderId()));
-            log.info("Published OrderCreatedEvent for orderId:{}", savedOrder.getOrderId());
         }
         return new OrderConfirmationDTO(savedOrder.getOrderId(), savedOrder.getStatus());
     }
 
     public Optional<Order> findOrderByOrderId(String orderId) {
         return this.orderRepository.findByOrderId(orderId);
-    }
-
-    public void cancelOrder(String orderId) {
-        log.info("Cancel order with OrderId: {}", orderId);
-        Order order = findOrderByOrderId(orderId).orElse(null);
-        if (order == null) {
-            throw new OrderNotFoundException(orderId);
-        }
-
-        if (order.getStatus() == OrderStatus.DELIVERED) {
-            throw new BadRequestException("Order is already delivered");
-        }
-        order.setStatus(OrderStatus.CANCELLED);
-        orderRepository.save(order);
-    }
-
-    public List<Order> findOrdersByStatus(OrderStatus status) {
-        return orderRepository.findByStatus(status);
     }
 
     public void updateOrderStatus(String orderId, OrderStatus status, String comments) {
